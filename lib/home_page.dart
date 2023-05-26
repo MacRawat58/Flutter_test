d872921c06693dbb5452369a1c34c4da97e52b58
MacRawat58 <96304497+MacRawat58@users.noreply.github.com>
2023-05-26 13:17:13 +0530

    Home pagecomponent

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Mac";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mac App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome, to $days days Course by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,14 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:Material (
-        child: Center(
-          child: Container(
-          child: Text("Welcome"),
-          ),
-        ), 
-    ),
-  );
+    return MaterialApp(home: HomePage());
   }
 }
